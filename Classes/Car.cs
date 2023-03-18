2ec0ba96271b5b2df3d92c803b887f9ab5692a5e
Victoria Pham <victoriapham52@gmail.com>
2023-03-17 22:55:52 -0700

    commit exercise one

---
+++ Classes/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Classes
+{
+	public class Car
+	{
+		public Car()
+		{
+        }
+
+    public string Make { get; set; } // Property
+    public string Model { get; set; } // Property
+	public int Year { get; set; } // Property
+
+
+	
+
+	}	
+}
+
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace Classes
+﻿using System.Reflection;
+
+namespace Classes
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var car = new Car()
+            {
+                Year = 2005,
+                Make = "Acura",
+                Model = "TSX",
+            };
+
+            Console.WriteLine($"{car.Year} {car.Make} {car.Model}");
+
         }
     }
 }
